366d3656f13fd9ab0075759b13d351d4ba4d0ede
Alisher <umarovalisher132@gmail.com>
2022-08-19 23:39:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function setClock() {
+    const courrentDate = new Date()
+    const secondsRatio = courrentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + courrentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + courrentDate.getHours()) / 12
+
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
